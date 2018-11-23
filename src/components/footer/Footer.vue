86d481b4a9cd60472457b7c37574d39e77c39418
Almantera Tiantana Al Faruqi <alman.alfaruq@gmail.com>
2018-11-24 00:30:49 +0700

    Change the description

--- src/components/footer/Footer.vue
+++ src/components/footer/Footer.vue
@@ -2,7 +2,7 @@
   <div class="footer has-text-centered">
     <div class="columns ">
       <div class="column">
-        <p>Phasellus feugiat arcu sapien, et iaculis ipsum elementum sit amet.</p>
+        <p>Created By: Axellageraldinc A., Almantera T. A. F., Azzum Jordhan W.</p>
       </div>
     <div class="column">
       <p>Seelih</p>
